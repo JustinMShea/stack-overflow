2f8a1ae7c240824b14ac94ae1998447f622a29c6
JustinMShea <jshea@mail.roosevelt.edu>
2017-08-06 13:54:49 -0500

    Transition to new file organization approach complete.

--- R/render-files-script.R
+++ R/render-files-script.R
@@ -1,20 +1,23 @@
 
 # Define Rmarkdown file path and directories
-name <- "multiple time series graphs in one plot disregarding empty fields.Rmd"
-file <- paste0(getwd(),"/Rmd/", name)
+file <- "R code inside math notation R Markdown.Rmd"
+file_path <- paste0(getwd(),"/Rmd/", file)
 
 github_dir <- paste0(getwd(),"/github_documents/")
 pdf_dir <- paste0(getwd(),"/pdf_documents/")
 html_dir <- paste0(getwd(),"/html_documents/")
 
-# Render Github document to github directory
-rmarkdown::render(file, output_format = "github_document", output_dir = github_dir)
 
-# Render pdf document to pdf directory
-rmarkdown::render(file, output_format = "pdf_document", output_dir = pdf_dir)
+# Render Github document to `github_documents``
+rmarkdown::render(file_path, output_format = "github_document", output_dir = github_dir)
 
-# move auto-generated html file to html_directory and delete from github directory
+# move auto-generated html file to `html_documents` 
 html_file <- paste0(getwd(),"/github_documents/",grep(".html",list.files(github_dir), value=TRUE))
 file.copy(html_file, html_dir, overwrite = TRUE)
-
+# Delete .html file from github directory
 file.remove(html_file)
+
+
+# Render pdf document to `pdf_documents`
+rmarkdown::render(file_path, output_format = "pdf_document", output_dir = pdf_dir)
+
